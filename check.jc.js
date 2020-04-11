4b78a4588de837c6edeabc1fa72c9b81aaaf63cc
dikshantsharma12 <63445571+dikshantsharma12@users.noreply.github.com>
2020-04-11 18:57:06 +0530

    Project

---
+++ check.jc.js
@@ -0,0 +1,7 @@
+function check()
+		{if(  document.getElementById("c").value = document.getElementById("d").value;)
+		{
+			window.alert("Starting and ending location should not be same")
+		}
+				
+			}
---
+++ swapValue.js
@@ -0,0 +1,5 @@
+function swapValues() {
+    var tmp = document.getElementById("c").value;
+    document.getElementById("c").value = document.getElementById("d").value;
+    document.getElementById("d").value = tmp;
+}
